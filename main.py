4447edc68c9cce0fdc31be3d55683ac85a869e3d
Dmitrijs Sedovs <91872683+shjunci@users.noreply.github.com>
2021-12-06 07:58:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def izdruka(daudzums, sar1):
+  for elem in range(0,daudzums):
+    print(sar1[elem])
+  return 0
+
+saraksts = [2,4,5,6,1,2,34,5]
+daudzums = int(input("Ievadi elementu skaitu:"))
+rez = izdruka(daudzums, saraksts)
